fe5a3942e5398656e33d3fda85970c89f2d56a28
joh21423 <joh21423@d.umn.edu>
2023-11-13 20:02:46 -0600

    just remove the login page imports in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:units/Pages/Auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';  // Make sure this import is correct
 
-Future<void> main() async  {
+void main() async  {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
