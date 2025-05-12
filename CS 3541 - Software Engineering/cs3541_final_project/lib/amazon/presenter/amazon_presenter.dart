1b481d1efd763221c41a60f03ed75946ed5e6958
Kane John <119974838+JOh2809@users.noreply.github.com>
2025-05-12 14:34:14 -0500

    Delete CS 3541 - Software Engineering directory

--- CS 3541 - Software Engineering/cs3541_final_project/lib/amazon/presenter/amazon_presenter.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ffi';
-
-class AmazonSearchPresenter {}
-class AmazonBookListPresenter {}
-class AmazonBookReviewsPresenter {}
-class AmazonGiveReviewPresenter {}
-
-class YouTubePresenter {
-}
